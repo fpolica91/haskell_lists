a1010dd49ffe6ffb5efa6e5af39f56a22f2c3c0c
Fabricio <39638775+fpolica91@users.noreply.github.com>
2021-07-19 02:30:30 +0000

    lists

--- src/Main.hs
+++ src/Main.hs
@@ -14,3 +14,28 @@ divisorHelper n i | n < i = 0
 
 divisor 1 = 1
 divisor n = divisorHelper n 1
+
+empty = []
+ints = [1,2,3]
+functions = [(+1), (*2)]
+nested = [["hello"], ["how are you"]]
+-- If x is an element of type a, and xs is a list of type [a], then l = x : xs
+containsMatchingInt :: (Int -> Bool) -> [Int] -> Bool
+containsMatchingInt _ [] = False
+
+
+-- test will be a function or operator
+-- example of how to run containsMatchingInt (>5) [2, -3, 7]
+-- another example containsMatchingInt (\x -> x `mod` 3 == 0) [-2, 4, 8)
+-- x is the first item of the list 
+-- xs refers to the remainder values
+containsMatchingInt test (x:xs)
+ | test x == True = True
+ | otherwise = containsMatchingInt test xs
+
+
+findInList _ [] =  error "empty list or number no in list"
+
+findInList number (x:xs)
+  | number == x = x
+  | otherwise =  findInList number xs                             
