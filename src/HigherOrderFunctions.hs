24786044341ed63c1942dd695968b5d9bbba3c5c
Fabricio <39638775+fpolica91@users.noreply.github.com>
2021-07-22 03:49:47 +0000

    fequency

--- src/HigherOrderFunctions.hs
+++ src/HigherOrderFunctions.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List
 
 roundList = map round
 
@@ -12,3 +13,19 @@ removePunctuation (x:xs)
 
 
 removePunctuationUsingFilter = filter (not . isPunctuation)
+ 
+clean = filter(\x -> isLetter x || x == ' ') . map toLower
+
+
+
+wordFrequencies s =
+   let cleaned = clean s
+       groupedWords = (group . sort . words) cleaned
+   in map (\xs -> (head xs, length xs)) groupedWords
+
+
+-- function maps over first item in tuple (hello, 5) 
+-- uses filter on the second item of the tuple 5 and returns it if greater than 5
+frequentWordsOnly = map fst .filter(\x -> snd x >= 5)
+
+--frequentWordsOnly [(hello, 7), (world, 3)]
